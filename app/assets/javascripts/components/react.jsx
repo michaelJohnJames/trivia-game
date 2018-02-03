9a9f4d24aa05b1d0f85e8de9e857d9a5ba48ee8e
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2018-02-03 08:11:19 -0500

    add link to landing page

--- app/assets/javascripts/components/react.jsx
+++ app/assets/javascripts/components/react.jsx
@@ -1,8 +1,10 @@
-class HelloWorld extends React.Component {
+class Title extends React.Component {
   render () {
     return (
       <div>
-        <h1>Greeting: {this.props.greeting}</h1>
+        <h1>{this.props.greeting}</h1>
+        <button><a href=""></a></button>
+        <button>High Scores</button>
       </div>
     );
   }
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,21 @@ class QuestionsController < ApplicationController
   def index
   end
 
+  def show
+  end
+
   def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
