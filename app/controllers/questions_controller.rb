1a2935555addb72714a277a635853e54dfcdb1fb
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2018-02-03 09:15:42 -0500

    add api to controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,13 @@
 class QuestionsController < ApplicationController
   def index
-  
 
+  @response = HTTParty.get("http://jservice.io/api/random.json").parsed_response
+#puts @response., @response.code, @response.message, @response.headers.inspect
+    #respond_to do |format|
+      #format.JSON { render :json => JSON.parse(@response) }
+      #format.html { render "index.html.erb"}
+    #end
+  @question = @response[0].question
   end
 
   def show
