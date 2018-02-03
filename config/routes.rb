335933b725244e202d982958bd064bad343bff1b
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2018-02-03 07:19:52 -0500

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  
+
   resources :questions
 
-  root 'welcome#index'
+  root 'welcomes#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
