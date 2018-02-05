8cd19d928fb479ecf57734529d9b3e79dd244f8c
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2018-02-05 17:17:43 -0500

    add category on questions component

--- app/assets/javascripts/components/question.jsx
+++ app/assets/javascripts/components/question.jsx
@@ -3,6 +3,7 @@ class Question extends React.Component {
     return (
       <div>
       <h3>This Question is worth {this.props.question[0].value} points!</h3>
+      <h3>Category: {this.props.question[0].category.title} </h3>
       <h1>{this.props.question[0].question}</h1>
       <p>{this.props.question[0].answer}</p>
 
