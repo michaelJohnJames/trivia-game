a0622f1661c9b5698aeb52881a126e1132a95677
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2018-02-06 14:40:35 -0500

    add submit function

--- app/assets/javascripts/components/answer.jsx
+++ app/assets/javascripts/components/answer.jsx
@@ -1,13 +1,39 @@
 class Answer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+
+    //var message = document.getElementById('msg');
+    //this.setState({value: event.target.value})
+    //console.log(guesses.value);
+    //console.log(this.props.answer[0].answer)
+    if ( guesses.value.includes(this.props.answer[0].answer)) {
+     console.log("You are correct!")
+    //  message.textContent = "You are correct!"
+    } else {
+      console.log("You are wrong")
+    //  message.textContent = "Sorry, the answer is " + {this.props.answer[0].answer}
+    }
+    e.preventDefault();
+  }
+
+
+
   render () {
     return (
       <div>
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label>Enter your answer:</label><br></br>
-        <input type="text"></input><br></br>
+        <input type="text" id="guesses" ></input><br></br>
         <input type="submit"></input>
       </form>
+      <p id="msg"></p>
       </div>
+
     );
   }
 }
