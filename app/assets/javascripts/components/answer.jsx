f3ce3f1afe19aa62df1b20b3892e340eab270f71
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2018-02-05 19:58:59 -0500

    change answer component

--- app/assets/javascripts/components/answer.jsx
+++ app/assets/javascripts/components/answer.jsx
@@ -2,8 +2,6 @@ class Answer extends React.Component {
   render () {
     return (
       <div>
-
-      <p>{this.props.answer[0].answer.HTML_safe}</p>
       <form>
         <label>Enter your answer:</label><br></br>
         <input type="text"></input><br></br>
