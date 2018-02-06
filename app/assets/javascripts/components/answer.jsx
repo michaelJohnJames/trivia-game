13e1ea538ea614d40c1b0d66e6ffb4239dc12b8e
michaelJohnJames <32465022+michaelJohnJames@users.noreply.github.com>
2018-02-06 16:55:50 -0500

    set question number

--- app/assets/javascripts/components/answer.jsx
+++ app/assets/javascripts/components/answer.jsx
@@ -1,7 +1,7 @@
 class Answer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {msg: ''};
+    this.state = {msg: '', counter: 0};
     this.checkAnswer = this.checkAnswer.bind(this);
   }
 
@@ -12,7 +12,9 @@ class Answer extends React.Component {
     //console.log(this.props.answer[0].answer)
     if ( this.props.answer[0].answer.includes(guesses.value)) {
         this.setState(prevState => ({
-          msg: "You are correct!"
+          msg: "You are correct!",
+          counter: this.state.counter + 1
+          //counter: {this.state.counter}++
         }));
      //this.state = {msg.value: "You are correct!"}
     //  message.textContent = "You are correct!"
@@ -21,10 +23,9 @@ class Answer extends React.Component {
         msg: "Sorry the answer is: " + this.props.answer[0].answer + "!"
       }))
       console.log("You are wrong");
-      console.log("msg.value");
     //  message.textContent = "Sorry, the answer is " + {this.props.answer[0].answer}
     }
-    e.preventDefault();
+    //e.preventDefault();
   }
 
 
@@ -32,13 +33,13 @@ class Answer extends React.Component {
   render () {
     return (
       <div>
+      <p id="q-counter">Question {this.state.counter} of 5</p>
       <form onSubmit={this.checkAnswer}>
         <label>Enter your answer:</label><br></br>
         <input type="text" id="guesses" ></input><br></br>
         <input type="submit"></input>
       </form>
-      <p>{this.state.msg}</p>
-
+      <p id="msg">{this.state.msg}</p>
       </div>
 
     );
